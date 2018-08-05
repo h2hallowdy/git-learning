911c5c01bb6dbfae77ff2108ecd3ad83ef227d28
Hoang <info@coders.tokyo>
2018-08-05 17:09:01 +0700

    Add weight for Mouse

--- Mouse.js
+++ Mouse.js
@@ -1,5 +1,11 @@
-function Mouse(name) {
+function Mouse(name, weight) {
 	this.name = name;
+	this.dead = false;
+	this.weight = weight;
+}
+
+Mouse.prototype.die = function() {
+	this.dead = true;
 }
 
 module.exports = Mouse;
